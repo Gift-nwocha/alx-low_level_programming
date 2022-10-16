aba00cd4ea29a75675478c951129c83d8781d9db
Gift-nwocha <nwochag14@gmail.com>
2022-10-16 06:23:24 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: 0 no error
+ **/
+int main(void)
+{
+	char i;
+	char j;
+
+	for (i = 'a'; i < = 'z'; i++)
+	{
+		putchar(i);
+	}
+	for (i = 'A'; j < = 'Z'; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
