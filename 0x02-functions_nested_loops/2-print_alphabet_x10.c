6acdee17bec2b127154d5851ca5b3ac829d44968
Gift-nwocha <nwochag14@gmail.com>
2022-10-18 19:26:50 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,6 +2,7 @@
 
 /**
  * print_alphabet_x10 - print 10 times the alphabet, in lowercase
+ * Return: 0 no error
  */
 
 void print_alphabet_x10(void)
@@ -16,4 +17,6 @@ void print_alphabet_x10(void)
 		_putchar(letter);
 
 	_putchar('\n');
+	return (0);
 	}
+}
