4a945835ab4b9f85f002b47cc1fdbe8146792a43
Gift-nwocha <nwochag14@gmail.com>
2022-10-18 17:42:54 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
-int _putchar(char c)
+int _putchar(char c);
+void print_alphabet(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
 
 #endif
