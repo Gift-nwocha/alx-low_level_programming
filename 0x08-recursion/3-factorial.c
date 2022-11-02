4b923eb146b690ae07753fe81f7e133e690f8b52
Gift-nwocha <nwochag14@gmail.com>
2022-11-02 16:44:28 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * fractional - returns the fractional of a given number
+ * @n: the number to find the fractional of 
+ *
+ * Return: fractional of the number
+ */
+int factorial(int n)
+{
+	int resul
