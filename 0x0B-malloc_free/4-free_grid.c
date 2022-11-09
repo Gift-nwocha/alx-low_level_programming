87ac68bc3fac45c5c2f9027240fc1613cd415107
Gift-nwocha <nwochag14@gmail.com>
2022-11-08 18:08:50 -0800

    4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -13,8 +13,8 @@ void free_grid(int **grid, int height)
 {
 	if (grid != NULL && height != 0)
 	{
-		for (height >= 0; height--)
-			frr(grid[height]);
+		for (; height >= 0; height--)
+			free(grid[height]);
 		free(grid);
 	}
 }
