2019bb817ce3d34e5a45e7aad4b2233a51254167
Gift-nwocha <nwochag14@gmail.com>
2022-11-08 17:34:17 -0800

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,45 +1,40 @@
 #include "main.h"
-/**
- * _strlen - count array
- * @s: array of an element
- * Return: 1
- */
-
-int _strlen(char *s)
-{
-	unsigned int i;
-
-	i = 0;
-	while (s[i] != '\0') /*count character of string*/
-	{
-		i++;
-	}
-	return (i);
-}
+#include <stdlib.h>
 
 /**
- * str_concat - back a pointer to an array
- * @s1: array one
- * @s2: array two
- * Return: always an array dynamic
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: pointer of an array of chars
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	char *dst;
-	unsigned int i, j;
+	char *strout;
+	unsigned int i, j, k, limit;
 
-	/*if the array is empty*/
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	/*count size total*/
-	for (i = 0; *(s1 + i) != '\0'; i++)
-		*(dst + i) = *(s1 + i);
-	for (j = 0; *(s2 + j) != '\0'; j++)
-		*(dst + i) = *(s2 + j);
-	i++;
-	return (dst);
+	for (i = 0; s1[i]; != '\0'; i++)
+		for (j = 0; s2[j]; != '\0'; j++)
+
+			strout = malloc(sizeof(char) * (i + j + l));
+
+	if (strout == NULL)
+	{
+		free(strout);
+		return (NULL);
+	}
+	for (k = 0; k < i; k++)
+		strout[k] = s1[k];
+
+	limit = j;
+	for (j = 0; j <= limit; k++, j++)
+		strout[k] = s2[j];
+
+	return (strout);
 }
