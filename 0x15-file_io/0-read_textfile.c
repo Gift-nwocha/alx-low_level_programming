4adae7c890dd5fb077c3de277feeef40af2e9f63
Gift-nwocha <nwochag14@gmail.com>
2022-12-05 16:47:42 -0800

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-read_textfile.c -o a

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,8 +7,7 @@
  * @letters: the number of letters the
  *           function should read and print
  * Return: if the function falls or filename is NULL - 0
- *         O/w - the actual number of bytes the function can read
- *         and print
+ *         O/w - the actual number of bytes the function read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -21,4 +20,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(o);
+
+	return (w);
 }
